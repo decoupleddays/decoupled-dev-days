c4bc6b4e40f29e4e8b4afbc3c0d28f60007fc578
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-05-01 08:09:04 -0700

    Cleans up code.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,23 +8,22 @@ import './header.scss'
 const Header = ({ siteTitle }) => (
   <div className="header">
     <div className="header__menu">
-      {' '}
-      {/** Menu to go here when we have more pages **/}{' '}
+      {/** Menu to go here when we have more pages **/}
       <Button>Register now</Button>
       <a href="" className="header__menu-register">
-        {' '}
-        Register Now{' '}
-      </a>{' '}
-    </div>{' '}
+        
+        Register Now
+      </a>
+    </div>
     <div className="header__hero">
       <h1>
         <Link to="/">
-          <span className="header__hero-site"> Decoupled Drupal Days </span>{' '}
-          <span className="header__hero-location"> New York City </span>{' '}
-          <span className="header__hero-date"> Aug.17 - 19, 2018 </span>{' '}
-        </Link>{' '}
-      </h1>{' '}
-    </div>{' '}
+          <span className="header__hero-site"> Decoupled Drupal Days </span>
+          <span className="header__hero-location"> New York City </span>
+          <span className="header__hero-date"> Aug.17 - 19, 2018 </span>
+        </Link>
+      </h1>
+    </div>
   </div>
 )
 
